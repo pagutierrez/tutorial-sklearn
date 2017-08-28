6fc8c69628100ecb5d6ae057e9d60daf3c635c5c
Pedro Antonio Gutierrez <pagutierrez@srvrrycinf05.priv.uco.es>
2017-08-28 18:39:07 +0200

    Incluidos datasets...

--- fetch_data.py
+++ fetch_data.py
@@ -1,4 +1,5 @@
-import os
+
+rt os
 import zipfile
 
 try:
@@ -11,6 +12,10 @@ import tarfile
 
 IMDB_URL = "http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
 IMDB_ARCHIVE_NAME = "aclImdb_v1.tar.gz"
+TITANIC_ARCHIVE_NAME = "titanic3.csv"
+TITANIC_URL = "https://raw.githubusercontent.com/amueller/scipy-2017-sklearn/master/notebooks/datasets/titanic3.csv"
+SPAM_ARCHIVE_NAME = "SMSSpamCollection"
+SPAM_URL = "https://raw.githubusercontent.com/amueller/scipy-2017-sklearn/master/notebooks/datasets/smsspam/SMSSpamCollection"
 
 
 def get_datasets_folder():
@@ -59,15 +64,48 @@ def check_imdb(datasets_folder):
     print("Comprobando que los directorios train y test del dataset IMDb existen...")
     assert os.path.exists(train_path)
     assert os.path.exists(test_path)
-    print("=> Success!")
+    print("=> Conseguido!")
 
+def check_titanic(datasets_folder):
+    print("\nComprobando la disponibilidad del dataset titanic")
+    archive_path = os.path.join(datasets_folder, TITANIC_ARCHIVE_NAME)
+
+    if not os.path.exists(archive_path):
+        print("Descargando el dataset de %s (104KB)" % TITANIC_URL)
+        opener = urlopen(TITANIC_URL)
+        open(archive_path, 'wb').write(opener.read())
+    else:
+        print("Archivo ya existe: " + archive_path)
+
+    print("=> Conseguido!")
+
+def check_sms(datasets_folder):
+    print("\nComprobando la disponibilidad del dataset SMS_SPAM")
+    directory_path = os.path.join(datasets_folder,'smsspam')
+    archive_path = os.path.join(directory_path, SPAM_ARCHIVE_NAME)
+
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+        if not os.path.exists(archive_path):
+            print("Descargando el dataset de %s (466KB)" % SPAM_URL)
+            opener = urlopen(SPAM_URL)
+            open(archive_path, 'wb').write(opener.read())
+        else:
+            print("Archivo ya existe: " + archive_path)
+    else:
+        print("Carpeta ya existe: " + directory_path)
+
+    print("=> Conseguido!")
 
 if __name__ == "__main__":
     datasets_folder = get_datasets_folder()
     check_imdb(datasets_folder)
+    check_titanic(datasets_folder)
+    check_sms(datasets_folder)
 
     print("\n Cargando Labeled Faces Data (~200MB)")
     from sklearn.datasets import fetch_lfw_people
     fetch_lfw_people(min_faces_per_person=70, resize=0.4,
                      data_home=datasets_folder)
     print("=> Conseguido!")
+
